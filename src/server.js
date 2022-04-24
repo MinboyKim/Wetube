855d3c892645f1100cedf43cb8988f2f63cbc7f0
MinboyKim <kdm3801@gmail.com>
2022-04-24 21:40:14 +0900

    0424 #5.1

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("I'm in the Middle!");
-    next();
-  },
-  (req, res, next) => res.send("Hi! D")
-);
+app.set("view engine", "pug");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on PORT http://localhost:${PORT}`);
