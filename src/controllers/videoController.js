fa09609390ea8dfc2a413385ed0ac0c4971f76db
MinboyKim <kdm3801@gmail.com>
2022-04-29 22:17:28 +0900

    0429 #5.2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,12 +2,10 @@ export const trending = (req, res) => {
   res.render("home");
 };
 export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
+  res.render("watch");
 };
 export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit");
+  res.render("edit");
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
